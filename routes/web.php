da82e931358434521edf4a4a4e4fe8c1ecc0e5e8
= <harits_nurrois@venteny.com>
2025-09-15 23:10:40 +0700

    feat: add home link for web

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return redirect('/soal1');
+});
+
 Route::get('/soal1', function () {
     return view('welcome');
 });
 
-Route::get('/soal2',function(){
+Route::get('/soal2', function () {
     return view('soal2');
 });
